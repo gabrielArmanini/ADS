ece5159b8faca4ada6a13ae0423bd848edcc45de
gabrielArmanini <gabrielarmanini@gmail.com>
2020-02-28 04:46:28 -0300

    Create exe1.c

---
+++ exe1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+
+	int valor1;
+	int valor2;
+	int PROD;
+
+	printf ("digite dois valores para a multiplicação:");
+	scanf ("%d","%d", &valor1, &valor2);
+
+	PROD = valor1 * valor2;
+
+	printf("PROD = ", PROD);
+
+return = 0
+}
+
